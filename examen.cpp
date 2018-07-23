603ab430db17551d565ddfaf598e6bfdd7e31e99
m16u3l <m16u3l_777@hotmail.com>
2018-07-22 23:29:00 -0400

    count letters and words

--- examen.cpp
+++ examen.cpp
@@ -8,6 +8,18 @@ class text_processor
     char* a;
     char* b;
     char* c;
+
+    struct parraf
+    {
+        size_t len;
+        char* words;
+    };
+
+    struct word
+    {
+        size_t len_word;
+        char* word;
+    };
 public:
 
     text_processor(const char* a, const char*b, const char* c)
@@ -31,7 +43,7 @@ public:
         delete[] b;
         delete[] c;
     }
-
+/*
     text_processor get_most_frequent_words()
     {
 
@@ -41,7 +53,7 @@ public:
     {
     
     }
-
+*/
     void print()
     {
         cout<< "hi" <<endl;
@@ -51,6 +63,63 @@ public:
     {
         return 0;
     }
+
+    int CountWords(const char* str)
+    {
+        if (str == NULL)
+            return 0;  // let the requirements define this...
+
+        bool inSpaces = true;
+        int numWords = 0;
+
+        while (*str) //!= nullptrf NULL
+        {
+            if (std::isspace(*str))
+            {
+                inSpaces = true;
+            }
+            else if (inSpaces)
+            {
+                numWords++;
+                inSpaces = false;
+            }
+            ++str;
+        }
+        return numWords;
+    }
+
+    int CountWordsAndLetters(char* str, int& words, int& letters)
+    {
+        words = 0;
+        unsigned int i = 0;
+        letters = 0;
+
+        while (str[i] == ' ')
+                i++;
+
+        for (; str[i]; i++)
+        {
+                if (((str[i] >= 'a') && (str[i] <= 'z')) || ((str[i] >= 'A') && (str[i] <= 'Z')))
+                        letters++;
+                if (str[i] == ' ')
+                {
+                        words++;
+                        while (1)
+                                if (str[i] == ' ')
+                                        i++;
+                                else
+                                {
+                                        i--;
+                                        break;
+                                }
+
+                }
+
+        }
+        words = words + 1;
+        return (words);
+    }
+
 
     size_t get_letter_count()
     {
@@ -71,10 +140,11 @@ public:
         return 0;
     }
 
+    /*
     text_processor add_word(const char* c)
     {
 
-    }
+    }*/
 };
 
 int main()
@@ -83,8 +153,8 @@ int main()
 	"the emulator runs in a just-in-time basis, converting blocks of x86 code to equivalent blocks of arm code. this conversion is cached both in memory (so each given part of a program only has to be translated once per run) and on disk (so subsequent uses of the program should be faster, as they can skip the translation). moreover, system libraries - the various dlls that applications load to make use of operating system feature - are all native arm code, including the libraries loaded by x86 programs. calling them compiled hybrid portable executables (or 'chippie' for short), these libraries are arm native code, compiled in such a way as to let them respond to x86 function calls.",
 	"while processor-intensive applications are liable to suffer a significant performance hit from this emulation - photoshop will work in the emulator, but it won't be very fast - applications that spend a substantial amount of time waiting around for the user - such as word - should perform with adequate performance. as one might expect, this emulation isn't available in the kernel, so x86 device drivers won't work on these systems. it's also exclusively 32-bit; software that's available only in a 64-bit x86 version won't be compatible. "};
     
-    ts.get_most_frequent_words().print();
-    ts.get_least_frequent_words().print();
+    //ts.get_most_frequent_words().print();
+    //ts.get_least_frequent_words().print();
     cout << ts.get_char_count() << "\n";
     cout << ts.get_letter_count() << "\n";
     cout << ts.get_digit_count() << "\n";
@@ -92,12 +162,12 @@ int main()
     cout << ts.get_frequency_by_word("emulator") << "\n";
     cout << ts.get_frequency_by_word("atari") << "\n";
     //
-    // ts.print_all_frequencies();
-    ts.add_word("asus").add_word("asus").add_word("asus").add_word("asus").add_word("asus").add_word("asus");
-    // // cout << "*****************\n";
-    // // ts.print_all_frequencies();
-    // /* should print something like:
-    //    hp: 2 occurrence(s)
+    //ts.print_all_frequencies();
+    //ts.add_word("asus").add_word("asus").add_word("asus").add_word("asus").add_word("asus").add_word("asus");
+    //cout << "*****************\n";
+    //ts.print_all_frequencies();
+    // should print something like:
+    //      hp: 2 occurrence(s)
     //    asus: 6 occurrence(s)
     //   while: 1 occurrence(s)
     return 0;
